1e3fc9486844681494492a9062e0e1013939ac90
brian.maina@student.moringaschool.com <brianmaina2>
2023-01-14 12:38:47 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Link from "./Links.js";
 
-function About() {
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Link github={props.githublink} l={props.linkedin} />
     </div>
   );
 }
